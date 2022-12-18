d228da2a25fceb41b8f7ceb10735318c2890b118
Aleksandr Pshikin <apshikin@internet.ru>
2022-12-18 16:29:23 +0300

    Add homework_3

---
+++ homework_3/task_1/main.cpp
@@ -0,0 +1,65 @@
+﻿#include <Windows.h>
+
+#include <iostream>
+
+class SmartArray // Класс  умный массив
+{
+private:
+	int sizeArr; // Заданный размер массива
+    int currentElement; // Индекс добавляемого элемента
+	int* arr; // Массив
+public:
+    SmartArray(int number) // Конструктор
+    {
+        sizeArr = number;
+        currentElement = 0;
+        arr = new int[number]();
+    }
+    ~SmartArray() // Деструктор
+    {
+        delete[] arr;
+    }
+    void addElement(int element) // Добавление элемента
+    {
+        if (currentElement >= sizeArr) // Если количество элементов больше количества элементов, на которую выделена память
+        {
+            throw std::exception("Попытка добавления элемента в уже заполненный массив");
+        }
+        else // Иначе добавляю элемент
+        {
+            arr[currentElement++] = element;
+        }
+    }
+    int getElement(int index) // Получение элемента массива
+    {
+        if (index >= sizeArr || index < 0) // Проверка на корректность индекса
+        {
+            throw std::exception("Элемента с данным индексом нет в массиве");
+        }
+        else
+        {
+            return arr[index];
+        }
+    }
+};
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    try {
+        SmartArray arr(5);
+        arr.addElement(1);
+        arr.addElement(4);
+        arr.addElement(155);
+        arr.addElement(14);
+        arr.addElement(15);
+        std::cout << arr.getElement(1) << std::endl;
+    }
+    catch (const std::exception& ex) {
+        std::cout << ex.what() << std::endl;
+    }
+
+    return 0;
+}
---
+++ homework_3/task_2/main.cpp
@@ -0,0 +1,101 @@
+﻿#include <Windows.h>
+
+#include <iostream>
+
+class SmartArray // Класс  умный массив
+{
+private:
+    int sizeArr; // Заданный размер массива
+    int currentElement; // Индекс добавляемого элемента
+    int* arr; // Массив
+public:
+    SmartArray(int number) // Конструктор
+    {
+        sizeArr = number;
+        currentElement = 0;
+        arr = new int[number]();
+    }
+    ~SmartArray() // Деструктор
+    {
+        delete[] arr;
+    }
+    void addElement(int element) // Добавление элемента
+    {
+        if (currentElement >= sizeArr) // Если количество элементов больше количества элементов, на которую выделена память
+        {
+            throw std::exception("Попытка добавления элемента в уже заполненный массив");
+        }
+        else // Иначе добавляю элемент
+        {
+            arr[currentElement++] = element;
+        }
+    }
+
+    // Геттеры
+    int getElement(int index) // Получение элемента массива
+    {
+        if (index >= sizeArr || index < 0) // Проверка на корректность индекса
+        {
+            throw std::exception("Элемента с данным индексом нет в массиве");
+        }
+        else
+        {
+            return arr[index];
+        }
+    }
+    int* getArr() // Получение массива
+    {
+        return arr;
+    }
+    int getSize() // Получение ращмера массива
+    {
+        return sizeArr;
+    }
+
+    SmartArray& operator= (const SmartArray& other) // Переопредление оператора присваивания
+    {
+        delete[] arr; // Освобождение памяти под теекущий массив
+        sizeArr = other.sizeArr; // Присвоение размера из заданного
+        currentElement = other.currentElement; // Присвоение текущего элемента из заданного
+        arr = new int[sizeArr]; // Создание нового массива
+        for (int i = 0; i < sizeArr; ++i) // Заполнение значениями
+        {
+            arr[i] = other.arr[i];
+        }
+        return *this; // Возвращение нового массива
+    }
+};
+
+void printArr(SmartArray& smartArr) // Печать массива
+{
+    for (int i = 0; i < smartArr.getSize(); ++i)
+    {
+        std::cout << smartArr.getElement(i) << " ";
+    }
+    std::cout << std::endl;
+}
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    SmartArray arr(5);
+    arr.addElement(1);
+    arr.addElement(4);
+    arr.addElement(155);
+
+    printArr(arr);
+
+    SmartArray new_array(2);
+    new_array.addElement(44);
+    new_array.addElement(34);
+
+    printArr(new_array);
+
+    arr = new_array;
+
+    printArr(arr);
+
+    return 0;
+}
